94381c12032856eac29920ce9a3776a57ae04d65
Chandler <use.an.alias@gmail.com>
2015-11-19 21:52:07 -0800

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
           return f.read()
 
 setup(name='markmywords',
-      version='0.1',
+      version='0.2',
       description='A text-generating Markov chain library',
       long_description=readme(),
       classifiers=[
